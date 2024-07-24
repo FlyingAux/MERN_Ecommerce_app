81541791bcd81229ccb35b1f085c66685d1b0105
rajdev04 <rajdevshrivastava72@gmail.com>
2024-07-24 15:19:24 +0530

    styling card or products

--- frontend/app/utils/productList/productaList.js
+++ frontend/app/utils/productList/productaList.js
@@ -9,11 +9,12 @@ const ProductaList = ({ product }) => {
       {product && (
         <>
           <h3>{product.title}</h3>
-          <p>{product.desc}</p>
+          {/* <p>{product.desc}</p> */}
           <h1>{product.price}</h1>
           <br/>
           {/* Add more product details as needed */}
-          <Link href=''>HELLO</Link>
+          <Link className='' id='btn_buy' href={`#!`}> Buy Now </Link>
+          <Link id='view_btn' href={`details/${product._id}`}> View </Link>
         </>
       )}
     </div>
