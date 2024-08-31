3d4c473be3e0014244399f4a6fe677e42c6a32d9
rajdev04 <rajdevshrivastava72@gmail.com>
2024-08-31 15:26:50 +0530

    ok

--- frontend/app/Components/partials/header.js
+++ frontend/app/Components/partials/header.js
@@ -5,11 +5,41 @@ import { RiMenu2Fill } from "react-icons/ri";
 import { IoMdClose } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
 import { GlobalState } from '@/app/GlobalState';
+import userApi from '@/app/api/userApi';
+import axios from 'axios';
 
 const header = () => {
 
     const state = useContext(GlobalState)
-    console.log(state)
+    const [ isLogged, setIsLogged ] = state.userApi.isLogged
+    const [ isAdmin, setIsAdmin ] = state.userApi.isAdmin
+
+
+    const logoutUser = async ()=>{
+        await axios.get('http://localhost:8000/user/logout')
+        localStorage.clear()
+        setIsAdmin(false)
+        setIsLogged(false)
+    }
+
+
+    const adminRouter = ()=>{
+        return(
+            <>
+                <i> <Link href='/create_product' className='not-italic font-bold capitalize'> Create Product </Link> </i>
+                <i> <Link href='/category' className='not-italic font-bold capitalize'> Categories </Link> </i>
+            </>
+        )
+    }
+
+    const loginRouter = ()=>{
+        return(
+            <>
+                <i> <Link href='/history' className='not-italic font-bold capitalize'> history </Link> </i>
+                <i> <Link href='/' className='not-italic font-bold capitalize' onClick={logoutUser}> Logout </Link> </i>
+            </>
+        )
+    }
 
   return (
     <>
@@ -19,26 +49,33 @@ const header = () => {
                 <RiMenu2Fill className='text-3xl'/>
             </div>
             <div>
-                <Link href='/' className='text-3xl font-semibold'>30DCSHOP</Link>
+                <Link href='/' className='text-3xl font-semibold'>{isAdmin?'Admin':'30DCSHOP'}</Link>
             </div>
             <div className=''>
 
                 <ul className='flex gap-5 items-center justify-center'>
                     <li>
-                    <Link href='/product' className='text-xl uppercase'>Products</Link>
+                    <Link href='/product' className='text-xl uppercase'>{isAdmin?'Shop':'PRODUCTS'}</Link>
                     </li>
-                    <li>
+
+                    {isAdmin && adminRouter()}
+                    {isLogged ? loginRouter() :  <li>
                     <Link href='/login' className='text-xl uppercase'>Register & Login</Link>
-                    </li>
+                    </li>}
+                   
                     <li hidden>
                     <IoMdClose className='text-3xl'/>
                     </li>
                 </ul>                
             </div>   
-            <div className='flex items-center justify-center gap-2'>
-                    <span className='text-xl'>0</span>
-                    <Link href='/cart'><IoCartOutline className='text-2xl' /></Link>
-                </div>
+
+            {
+                isAdmin ? '' : <div className='flex items-center justify-center gap-2'>
+                <span className='text-xl'>0</span>
+                <Link href='/cart'><IoCartOutline className='text-2xl' /></Link>
+            </div>
+            }
+            
         </div>
        </div>
     </>
